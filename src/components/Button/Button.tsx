49f45e57950accd65929d64d300f65873acd1a12
Andrii <alutsenko@spin.ai>
2022-06-28 10:34:49 +0300

    commit code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {Button} from 'antd';
 
 export interface ButtonProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const AntdButton = (props: ButtonProps) => {
+  return <Button>{props.label}</Button>;
 };
 
-export default Button;
+export default AntdButton;
